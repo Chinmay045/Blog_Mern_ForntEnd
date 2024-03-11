9b764cd2524bf2ac8e8b94f1ac25338bab1f389e
Chinmay <chinz.sandur07@gmail.com>
2024-03-11 17:24:06 +0530

    add tabs section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { AppBar, Box, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const [value, setValue] = useState();
@@ -11,14 +12,14 @@ export default function Header() {
                 <Typography variant='h5'>Blogs App</Typography>
                 <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
                     <Tabs textColor='inherit' value={value} onChange={(e, val) => setValue(val)}>
-                        <Tab label="All-Blogs" />
-                        <Tab label="My-Blogs" />
+                        <Tab LinkComponent={Link} to="/blogs" label="All-Blogs" />
+                        <Tab LinkComponent={Link} to="/myBlogs" label="My-Blogs" />
                     </Tabs>
                 </Box>
                 <Box display="flex" marginLeft="auto">
-                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
-                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>SignUp</Button>
-                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
+                    <Button  LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+                    <Button  LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>SignUp</Button>
+                    <Button  LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
                 </Box>
             </Toolbar>
         </AppBar>
