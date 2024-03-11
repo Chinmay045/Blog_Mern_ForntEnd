8f8e914108dc3b4adebdbddd38192b5dffc2ec74
Chinmay <chinz.sandur07@gmail.com>
2024-03-11 17:05:48 +0530

    add tabs section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, Box, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
 
 export default function Header() {
+    const [value, setValue] = useState();
     return (
         <AppBar sx={{ background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,21,222,1) 42%, rgba(0,212,255,1) 100%);" }}
             position='sticky'>
 
             <Toolbar>
                 <Typography variant='h5'>Blogs App</Typography>
+                <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
+                    <Tabs textColor='inherit' value={value} onChange={(e, val) => setValue(val)}>
+                        <Tab label="All-Blogs" />
+                        <Tab label="My-Blogs" />
+                    </Tabs>
+                </Box>
                 <Box display="flex" marginLeft="auto">
                     <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
                     <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>SignUp</Button>
+                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
                 </Box>
             </Toolbar>
         </AppBar>
