b7415e52f742e48957a696795d3b243f60141e64
Chinmay <chinz.sandur07@gmail.com>
2024-03-11 15:32:13 +0530

    add header component

---
+++ src/components/AddBlock.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddBlock() {
+    return (
+        <div>AddBlock</div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+
+export default function Header() {
+    return (
+        <AppBar sx={{ background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,21,222,1) 42%, rgba(0,212,255,1) 100%);" }}>
+            <Toolbar>
+                <Typography variant='h5'>Blogs App</Typography>
+                <Box display="flex" marginLeft="auto">
+                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+                    <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>SignUp</Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
